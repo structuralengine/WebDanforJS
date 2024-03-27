80cb66533d3186135ff44ae8526aa3bdf5130ec1
ducanhtuan <135207026+ducanhtuan@users.noreply.github.com>
2024-03-27 08:35:47 +0700

    change guide reiforcing bar

--- src/app/components/modal-preview/modal-preview.component.ts
+++ src/app/components/modal-preview/modal-preview.component.ts
@@ -1,50 +1,53 @@
 import { Component, EventEmitter, HostListener, Input, OnInit, Output, SimpleChanges, } from '@angular/core';
 import { DataHelperModule } from 'src/app/providers/data-helper.module';
-import { TranslateService } from '@ngx-translate/core';
-
-
-
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-modal-preview',
   templateUrl: './modal-preview.component.html',
   styleUrls: ['./modal-preview.component.scss']
 })
 export class ModalPreview implements OnInit {
-  @Input() show: boolean = false;
   @Input() element: any ;
   @Output() closeModal = new EventEmitter<any>()
   public dataDropDown=[
-    {
-      id:1,
-      title:"modal_preview.Rectangle"
+    {      
+      title:"modal_preview.Rectangle", 
+      image: "Rectangle"
     },
     {
-      id:2,
-      title:"modal_preview.TShape"
+      title:"modal_preview.TShape",
+      image:"Tshape"
     },
     {
-      id:3,
-      title:"modal_preview.Circle"
+      title:"modal_preview.Circle",
+      image:"Circle"
     },
     {
-      id:4,
-      title:"modal_preview.Ring"
+      title:"modal_preview.Ring",
+      image:"Ring"
     },
     {
-      id:5,
-      title:"modal_preview.HorizontalOval"
+      title:"modal_preview.HorizontalOval",
+      image:"HorizontalOval"
     },
     {
-      id:6,
-      title:"modal_preview.VerticalOval"
+      title:"modal_preview.VerticalOval",
+      image:"VerticalOval"
     },
   ]
   public select:string = "modal_preview.Rectangle";
   public checkShowDropDown:boolean= false
+  public imgLink: string;
+  public currentLang: string;
+  public folder: string = "member-guide"
   constructor(
     private helper: DataHelperModule,
     private translate: TranslateService,
-  ) { }
+  ) { 
+    this.translate.onLangChange.subscribe(() => {
+      this.getLink("Rectangle");
+    });
+  }
   @HostListener("document:click", ["$event"])
   public mouseClick(event: any){
     if(event.target?.id!=="text-b" && event.target?.id!=="icon-b" && event.target?.id!=="button-show"){
@@ -52,22 +55,81 @@ export class ModalPreview implements OnInit {
     }
   }
   ngOnInit() {
+    this.getLink("Rectangle");
   }
   ngOnChanges(changes: SimpleChanges): void {
-      if('show' in changes && changes["show"].currentValue){        
+      if('element' in changes){
+        switch(changes['element'].currentValue.iconId){
+          case "#member-dev-question-ax":{
+            this.folder = "reinforcing-bar/axial-bar";
+            this.dataDropDown=[
+              {
+                title:"modal_preview.Rectangle", 
+                image: "Rectangle"
+              },
+              {
+                title:"modal_preview.TShape",
+                image:"Tshape"
+              },
+              {
+                title:"modal_preview.CircleRing",
+                image:"Circle-Ring"
+              },              
+              {
+                title:"modal_preview.HorizontalOval",
+                image:"HorizontalOval"
+              },
+              {
+                title:"modal_preview.VerticalOval",
+                image:"VerticalOval"
+              },
+            ]
+            break;
+          }
+          case "#member-dev-question-la":{
+            this.folder = "reinforcing-bar/lateral-bar";
+            this.dataDropDown=[
+              {
+                title:"modal_preview.Rectangle", 
+                image: "Rectangle"
+              },
+              {
+                title:"modal_preview.TShape",
+                image:"Tshape"
+              },                      
+              {
+                title:"modal_preview.HorizontalOval",
+                image:"HorizontalOval"
+              },
+              {
+                title:"modal_preview.VerticalOval",
+                image:"VerticalOval"
+              },
+            ]
+            break;
+          }
+          default:{
+            this.folder = "member-guide"
+            break;
+          }
+        }
       }
   }
   closePreview(){
-    this.show = false;
     const member_dev = window.document.querySelector(this.element.iconId);
     member_dev.classList.remove("activeQ")
     this.closeModal.emit(true)
   }
   handleSelectItem(data:any){
     this.select=this.translate.instant(data.title);
-    this.checkShowDropDown=false
+    this.checkShowDropDown=false;
+    this.getLink(data.image);
   }
   showDropDown(){
     this.checkShowDropDown=!this.checkShowDropDown
+  }
+  getLink(name: string){
+    this.currentLang = this.translate.currentLang;
+    this.imgLink = `assets/img/${this.folder}/${this.currentLang}/${name}.svg`;
   }
 }
