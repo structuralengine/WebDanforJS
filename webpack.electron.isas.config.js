6ce782a6eb4924c583c469b584970255eac8db17
DuongTuanAnh1811 <anhdt@harmony-at.vn>
2024-05-29 15:35:21 +0700

    change isas.ts to main.ts

--- webpack.electron.isas.config.js
+++ webpack.electron.isas.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ...baseConfig,
   mode: 'production',
   entry: {
-    main: path.join(src, 'isas.ts')
+    main: path.join(src, 'main.ts')
   },
   output: {
     path: path.join(process.cwd(), 'dist'),
