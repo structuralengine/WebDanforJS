8fa2a0fa822dd5efd8ad03745b83e079b213742e
DuongTuanAnh1811 <anhdt@harmony-at.vn>
2024-06-18 15:12:12 +0700

    draw point oval

--- src/app/components/three/geometry/three-node.service.ts
+++ src/app/components/three/geometry/three-node.service.ts
@@ -604,14 +604,25 @@ export class ThreeNodeService {
       return (accumulator > element) ? accumulator : element
     });
     this.scale = max_val / 88;
+    this.type = memB < memH ? "Vertical" : "Horizontal"
     if (this.type === "Vertical") {
       this.createOval(memB / this.scale, memH / this.scale, 0xb9b9b9, this.type);
       this.createLineOval(memB / this.scale, memH / this.scale, 10 / this.scale, 0x333D46, this.type)
-      this.createLineDashedOval(memB / this.scale, memH / this.scale, 20 / this.scale, 0x333D46, this.type)
+      this.createLineDashedOval(memB / this.scale, memH / this.scale, 0x333D46, this.type)
+      this.dataRebar.selectedCalPoint.rebar0.map((data) => {
+        if (data.rebar_type === 2 || data.rebar_type === 3) {
+          this.createArcDashedOVal(memB / this.scale, memH / this.scale, data.cover / this.scale, 0x333D46, this.type, data.rebar_type)
+        }
+      })
     } else {
       this.createOval(memB / this.scale, memH / this.scale, 0xb9b9b9, this.type)
       this.createLineOval(memB / this.scale, memH / this.scale, 10 / this.scale, 0x333D46, this.type)
-      this.createLineDashedOval(memB / this.scale, memH / this.scale, 20 / this.scale, 0x333D46, this.type)
+      this.createLineDashedOval(memB / this.scale, memH / this.scale, 0x333D46, this.type)
+      this.dataRebar.selectedCalPoint.rebar0.map((data) => {
+        if (data.rebar_type === 5) {
+          this.createArcDashedOVal(memB / this.scale, memH / this.scale, data.cover / this.scale, 0x333D46, this.type, data.rebar_type)
+        }
+      })
     }   
     this.scene.render()
   }
@@ -627,12 +638,18 @@ export class ThreeNodeService {
       circleGeometry1.position.set(0, Math.abs(h / 2 - b / 2), 0);
       circleGeometry2 = new THREE.Mesh(new THREE.CircleGeometry(b / 2, 100, Math.PI, Math.PI), materialCircle);
       circleGeometry2.position.set(0, -Math.abs(h / 2 - b / 2), 0);
+      this.drawPointOval(b, h, 2, type)
+      this.drawPointOval(b, h, 3, type)
+      this.drawPointOval(b, h, 6, type)
     } else {
       geometry = new THREE.PlaneGeometry(Math.abs(h - b), h);
       circleGeometry1 = new THREE.Mesh(new THREE.CircleGeometry(h / 2, 100, Math.PI / 2, Math.PI), materialCircle);
       circleGeometry1.position.set(-Math.abs(h / 2 - b / 2), 0, 0);
       circleGeometry2 = new THREE.Mesh(new THREE.CircleGeometry(h / 2, 100, 3 / 2 * Math.PI, Math.PI), materialCircle);
       circleGeometry2.position.set(Math.abs(h / 2 - b / 2), 0, 0);
+      this.drawPointOval(b, h, 0, type)
+      this.drawPointOval(b, h, 1, type)
+      this.drawPointOval(b, h, 5, type)
     }
     const plane = new THREE.Mesh(geometry, materialPlane);
     plane.position.set(0, 0, 0);
@@ -640,6 +657,8 @@ export class ThreeNodeService {
     this.scene.add(plane);
     this.scene.add(circleGeometry1);
     this.scene.add(circleGeometry2);
+    this.scene.render()
+    
   }
   createLineOval(b: any, h: any, range: any, color: any, type) {
     const material = new THREE.LineBasicMaterial({ color: color })
@@ -745,89 +764,81 @@ export class ThreeNodeService {
     this.drawLineDim(jsonData["3"], jsonData["4"], 1, Math.round(b * this.scale), false, y_start / 2 + 2, 8, 1);
 
   }
-  createLineDashedOval(b: any, h: any, range: any, color: any, type) {
+  createArcDashedOVal(b: any, h: any, range: any, color: any, type, rebarType:any){
     const material = new THREE.LineBasicMaterial({ color: color })
-    const materialLine = new THREE.LineDashedMaterial({ color: color, dashSize: 1.5, gapSize: 1.5 })
 
-    let pointsLine1: any = []
-    let pointsLine2: any = []
+    let points: any
+    let curve: any
+    let ellipse: any
 
-    let points1: any
-    let curve1: any
-    let ellipse1: any
-    let line1: any
-
-    let points2: any
-    let curve2: any
-    let ellipse2: any
-    let line2: any
-
-    let points3: any
-    let curve3: any
-    let ellipse3: any
-
-    let points4: any
-    let curve4: any
-    let ellipse4: any
     if (type === "Vertical") {
-      curve1 = new THREE.EllipseCurve(
-        0, Math.abs(h / 2 - b / 2),
-        Math.abs(b / 2 - range), Math.abs(b / 2 - range),
-        0, Math.PI,
-        false,
-        0
-      );
-      curve2 = new THREE.EllipseCurve(
-        0, Math.abs(h / 2 - b / 2),
-        Math.abs(b / 2 - range - range * 2), Math.abs(b / 2 - range - range * 2),
-        0, Math.PI,
-        false,
-        0
-      );
-      curve3 = new THREE.EllipseCurve(
-        0, -Math.abs(h / 2 - b / 2),
-        Math.abs(b / 2 - range), Math.abs(b / 2 - range),
-        Math.PI, 2 * Math.PI,
-        false,
-        0
-      );
-      curve4 = new THREE.EllipseCurve(
-        0, -Math.abs(h / 2 - b / 2),
-        Math.abs(b / 2 - range - range * 2), Math.abs(b / 2 - range - range * 2),
-        Math.PI, 2 * Math.PI,
-        false,
-        0
-      );
-      pointsLine1.push(new THREE.Vector3(Math.abs(b / 2 + 20 / this.scale), Math.abs(h / 2 - b / 2), 0));
-      pointsLine1.push(new THREE.Vector3(-Math.abs(b / 2 + 20 / this.scale), Math.abs(h / 2 - b / 2), 0));
+      if(rebarType===2){
+        curve = new THREE.EllipseCurve(
+          0, Math.abs(h / 2 - b / 2),
+          Math.abs(b / 2 - range), Math.abs(b / 2 - range),
+          0, Math.PI,
+          false,
+          0
+        );
+      }
+      if(rebarType==3){
+        curve = new THREE.EllipseCurve(
+          0, -Math.abs(h / 2 - b / 2),
+          Math.abs(range - (h - b / 2)), Math.abs(range - (h - b / 2)),
+          Math.PI, 2 * Math.PI,
+          false,
+          0
+        );
+      }
 
-      pointsLine2.push(new THREE.Vector3(Math.abs(b / 2 + 20 / this.scale), -Math.abs(h / 2 - b / 2), 0));
-      pointsLine2.push(new THREE.Vector3(-Math.abs(b / 2 + 20 / this.scale), -Math.abs(h / 2 - b / 2), 0));
-
-      points2 = curve2.getPoints(100);
-      ellipse2 = new THREE.LineSegments(new THREE.BufferGeometry().setFromPoints(points2), material);
-
-      points4 = curve4.getPoints(100);
-      ellipse4 = new THREE.LineSegments(new THREE.BufferGeometry().setFromPoints(points4), material);
-      
-      this.scene.add(ellipse2)
-      this.scene.add(ellipse4)
     } else {
-      curve1 = new THREE.EllipseCurve(
+      let points2: any
+      let curve2: any
+      let ellipse2: any
+      curve = new THREE.EllipseCurve(
         -Math.abs(h / 2 - b / 2), 0,
         Math.abs(h / 2 - range), Math.abs(h / 2 - range),
         Math.PI / 2, 3 * Math.PI / 2,
         false,
         0
       );
-      curve3 = new THREE.EllipseCurve(
+      curve2 = new THREE.EllipseCurve(
         Math.abs(h / 2 - b / 2), 0,
         Math.abs(h / 2 - range), Math.abs(h / 2 - range),
         3 * Math.PI / 2, 5 * Math.PI / 2,
-
         false,
         0
       );
+
+      points2 = curve2.getPoints(100);
+      ellipse2 = new THREE.LineSegments(new THREE.BufferGeometry().setFromPoints(points2), material);
+
+      this.scene.add(ellipse2)
+    
+    }
+
+    points = curve.getPoints(100);
+    ellipse = new THREE.LineSegments(new THREE.BufferGeometry().setFromPoints(points), material);
+    
+    this.scene.add(ellipse)
+  }
+  createLineDashedOval(b: any, h: any, color: any, type) {
+    const materialLine = new THREE.LineDashedMaterial({ color: color, dashSize: 1.5, gapSize: 1.5 })
+
+    let pointsLine1: any = []
+    let pointsLine2: any = []
+
+    let line1: any
+    let line2: any
+
+    if (type === "Vertical") {
+      pointsLine1.push(new THREE.Vector3(Math.abs(b / 2 + 20 / this.scale), Math.abs(h / 2 - b / 2), 0));
+      pointsLine1.push(new THREE.Vector3(-Math.abs(b / 2 + 20 / this.scale), Math.abs(h / 2 - b / 2), 0));
+
+      pointsLine2.push(new THREE.Vector3(Math.abs(b / 2 + 20 / this.scale), -Math.abs(h / 2 - b / 2), 0));
+      pointsLine2.push(new THREE.Vector3(-Math.abs(b / 2 + 20 / this.scale), -Math.abs(h / 2 - b / 2), 0));
+    } else {
+      
       pointsLine1.push(new THREE.Vector3(Math.abs(h / 2 - b / 2), Math.abs(h / 2 + 20 / this.scale)));
       pointsLine1.push(new THREE.Vector3(Math.abs(h / 2 - b / 2), -Math.abs(h / 2 + 20 / this.scale)));
 
@@ -846,19 +857,91 @@ export class ThreeNodeService {
       materialLine
     );
     line2.computeLineDistances()
-
-    points1 = curve1.getPoints(100);
-    ellipse1 = new THREE.LineSegments(new THREE.BufferGeometry().setFromPoints(points1), material);
-
-    points3 = curve3.getPoints(100);
-    ellipse3 = new THREE.LineSegments(new THREE.BufferGeometry().setFromPoints(points3), material);
-
-    this.scene.add(ellipse1)
-    this.scene.add(ellipse3)
     this.scene.add(line1)
     this.scene.add(line2)
   }
+  drawPointOval(b: any, h: any, typeRebar: any,type:any){
+    let dataPoint = []
+    let color = typeRebar === 5|| typeRebar === 6 ? 0xfafafa : 0x000000
+    this.dataRebar.selectedCalPoint.rebar0.map((data) => {
+      if (data.rebar_type === typeRebar) {
+        dataPoint.push(data)
+      }
+    })
+    dataPoint.map((data, index) => {
+      this.geometry = new THREE.SphereBufferGeometry(data.dia / 2 / this.scale)
+      let interval = 0
+      if (typeRebar===5){
 
+      }
+      for (let i = 1; i <= data.quantity; i++) {
+        const mesh = new THREE.Mesh(this.geometry,
+          new THREE.MeshBasicMaterial({ color: color }));
+        const mesh2 = new THREE.Mesh(this.geometry,
+          new THREE.MeshBasicMaterial({ color: color }));
+        if (typeRebar === 6 && type === "Vertical") {
+          mesh.name = 'node6' + index + i;
+          mesh.position.x = -(b / 2 - data.dist_side / this.scale - interval);
+          mesh.position.y = h / 2 - data.dist_top / this.scale;
+          mesh.position.z = 0;
+          this.nodeList.children.push(mesh);
+          interval += (b - 2 * data.dist_side / this.scale) / (data.quantity - 1);
+        }
+        if ((typeRebar === 2 || typeRebar === 3  ) && type === "Vertical") {
+          mesh.name = typeRebar === 2 ? 'node2' + index + i : 'node3' + index + i;
+          let centerY = typeRebar === 2 ? h / 2 - b / 2 : -(h / 2 - b / 2)
+          let radius = typeRebar === 2 ? b / 2 - data.cover / this.scale : data.cover / this.scale - (h - b / 2)
+          let coordinate = this.getPointOnCircle(0, centerY, radius , interval);
+          mesh.position.x = -coordinate.x;
+          mesh.position.y = coordinate.y;
+          mesh.position.z = coordinate.z;
+          this.nodeList.children.push(mesh);
+          if(typeRebar===2){
+            interval += Math.PI / (data.quantity - 1) 
+          }else{
+            interval -= Math.PI / (data.quantity - 1) 
+          }
+        }
+        if ((typeRebar === 0 || typeRebar === 1) && type !== "Vertical") {
+          mesh.name = typeRebar === 0 ? 'node0' + index + i : 'node1' + index + i;
+          mesh.position.x = -(b / 2 - h / 2 - data.dist_side / this.scale - interval);
+          mesh.position.y = h / 2 - data.dist_top / this.scale;
+          mesh.position.z = 0;
+          this.nodeList.children.push(mesh);
+          interval += (b - h - 2 * data.dist_side / this.scale) / (data.quantity - 1);
+        }
+        if (typeRebar === 5 && type !== "Vertical") {
+          mesh.name =  'node5_1' + index + i 
+          mesh2.name = 'node5_2' + index + i 
+          let centerX1 =  -(h / 2 - b / 2) 
+          let centerX2 = h / 2 - b / 2
+          let radius = h / 2 - data.cover / this.scale 
+          let coordinate1 = this.getPointOnCircle(centerX1, 0, radius, interval - Math.PI / 2);
+          let coordinate2 = this.getPointOnCircle(centerX2, 0, radius, interval + Math.PI / 2);
+
+          mesh.position.x = -coordinate1.x;
+          mesh.position.y = coordinate1.y;
+          mesh.position.z = coordinate1.z;
+
+          mesh2.position.x = -coordinate2.x;
+          mesh2.position.y = coordinate2.y;
+          mesh2.position.z = coordinate2.z;
+          this.nodeList.children.push(mesh);
+          this.nodeList.children.push(mesh2);
+          interval += Math.PI / (data.quantity - 1)
+        }
+      }
+    })
+    
+    console.log("nodeList", this.nodeList)
+    console.log("b, h", b,h)
+  }
+public getPointOnCircle(centerX, centerY, radius, angle) {
+  const x = centerX + radius * Math.cos(angle);
+  const y = centerY + radius * Math.sin(angle);
+  const z = 0
+  return { x, y, z };
+}
   createDemoCircleRing() {
     var member = this.memmber.getData(this.dataRebar.selectedCalPoint.m_no);
     let memH = member['H'];
